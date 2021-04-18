216bd20c1b3e246b00dbc531d0e8878b970aff1b
Shivam Gupta <shivamg@nova.tech>
2021-04-18 20:48:27 +0530

    tasks on the blockchain

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -5,4 +5,22 @@ contract TodoList{
    //state variable 
    uint public taskCount = 0;
 
+   struct Task {
+       uint id;
+       string content;
+       bool completed;
+   }
+
+   mapping(uint => Task) public tasks;
+
+   function createTask(string memory _content) public{
+     taskCount++;
+     tasks[taskCount]=Task(taskCount, _content,false);
+   }
+
+   constructor() public {
+       createTask("Create a blockchain project");
+       createTask("Implement blockchain in an actual enterprise project");
+   }
+
 }
